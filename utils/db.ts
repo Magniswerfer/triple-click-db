a326af772d28e4d33f77042f38f8b9f768f000c7
Magnus HK <magnushoholt@pm.me>
2024-10-30 23:43:11 +0100

    different env stuff?

--- utils/db.ts
+++ utils/db.ts
@@ -1,6 +1,10 @@
-import "$std/dotenv/load.ts";  // Add this line at the top!
+import "$std/dotenv/load.ts";
 
-const url = Deno.env.get("DENO_KV_URL");
+const isDeploy = Deno.env.has("DENO_DEPLOYMENT_ID");
+const url = isDeploy 
+  ? "https://api.deno.com/databases/fa5b123c-7c5f-4dcc-8669-db5d041d2061/connect"  // Your database URL
+  : Deno.env.get("DENO_KV_URL");
+
 if (!url) {
   throw new Error("DENO_KV_URL environment variable is not set");
 }
